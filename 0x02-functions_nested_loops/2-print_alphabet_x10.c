5e53a610bf889db68a41db0a0322d517680528ba
Ifedayo <ifedayo_folorunsho@yahoo.com>
2022-10-19 15:37:57 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * main - checks code
+ *
+ * Returns void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char alpha = 'a';
+	
+	while (round < 10) /*print lowercases a-z*/
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+	
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
